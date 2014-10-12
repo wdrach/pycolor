339230299dda15d8c91a5db9db7de8e951e636bb
Merglyn <will.drach@gmail.com>
2014-10-11 19:52:16 -0600

    Added setup.py
    Added distutils setup.py (untested)

---
+++ setup.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='Pycolor',
+      version='1.0',
+      description='Ansi color for python',
+      author='Will Drach',
+      author_email='will.drach@live.com',
+      url='http://www.drach.co/pycolor',
+      packages=['pycolor.py'])
